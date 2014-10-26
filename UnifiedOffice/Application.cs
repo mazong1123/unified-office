d8cc6d2d78a5dc20cc39f1b1e8adda6c5bdff0b5
mazong1123 <mazong1123@gmail.com>
2014-10-26 11:08:47 +0800

    Changed project settings.

---
+++ UnifiedOffice/Application.cs
@@ -0,0 +1,41 @@
+﻿using Word = Microsoft.Office.Interop.Word;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UnifiedOffice.Word
+{
+    public class Application
+    {
+        private Word.Application app = null;
+        private WdAlertLevel displayAlerts = WdAlertLevel.wdAlertsAll;
+
+        public Application()
+        {
+            this.app = new Word.Application();
+        }
+
+        public WdAlertLevel DisplayAlerts
+        {
+            get
+            {
+                return this.displayAlerts;
+            }
+
+            set
+            {
+                this.displayAlerts = value;
+            }
+        }
+
+        public void Quit()
+        {
+            if (this.app != null)
+            {
+                this.app.Quit();
+                this.app = null;
+            }
+        }
+    }
+}
---
+++ UnifiedOffice/WdAlertLevel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UnifiedOffice.Word
+{
+    // Summary:
+    //     Specifies the way certain alerts and messages are handled while a macro is
+    //     running.
+    public enum WdAlertLevel
+    {
+        // Summary:
+        //     Only message boxes are displayed; errors are trapped and returned to the
+        //     macro.
+        wdAlertsMessageBox = -2,
+        //
+        // Summary:
+        //     All message boxes and alerts are displayed; errors are returned to the macro.
+        wdAlertsAll = -1,
+        //
+        // Summary:
+        //     No alerts or message boxes are displayed. If a macro encounters a message
+        //     box, the default value is chosen and the macro continues.
+        wdAlertsNone = 0,
+    }
+}
