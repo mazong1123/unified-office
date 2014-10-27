d6974fb4662e47903b976ba97bdfe239b0b03510
mazong1123 <mazong1123@gmail.com>
2014-10-28 00:13:57 +0800

    Added a documents property.

--- UnifiedOffice/Application.cs
+++ UnifiedOffice/Application.cs
@@ -9,10 +9,28 @@ namespace UnifiedOffice.Word
     public class Application
     {
         private InteropWord.Application app = null;
+        private Document activeDocument = null;
+        private IList<Document> documents = null;
 
         public Application()
         {
             this.app = new InteropWord.Application();
+            this.activeDocument = new Document(this.app.ActiveDocument);
+
+            this.documents = new List<Document>();
+            foreach (var interopDoc in this.app.Documents)
+            {
+                Document doc = new Document((InteropWord.Document)interopDoc);
+                this.documents.Add(doc);
+            }
+        }
+
+        public Document ActiveDocument
+        {
+            get 
+            {
+                return this.activeDocument;
+            }
         }
 
         public WdAlertLevel DisplayAlerts
