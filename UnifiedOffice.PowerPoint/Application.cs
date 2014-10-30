86ac30578ac72b8cc2da340951ff2982b0e9846f
mazong1123 <mazong1123@gmail.com>
2014-10-30 18:05:51 +0800

    Added PowerPoint library.

---
+++ UnifiedOffice.PowerPoint/Application.cs
@@ -0,0 +1,21 @@
+﻿using InteropPowerPoint = Microsoft.Office.Interop.PowerPoint;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UnifiedOffice.PowerPoint
+{
+    /// <summary>
+    /// Represents the Microsoft Office PowerPoint application.
+    /// </summary>
+    public class Application
+    {
+        private InteropPowerPoint.Application app = null;
+
+        public Application()
+        {
+            this.app = new InteropPowerPoint.Application();
+        }
+    }
+}
