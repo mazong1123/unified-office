2e335faec9b874361be844e175d61f70425dac58
mazong1123 <mazong1123@gmail.com>
2014-11-06 17:42:26 +0800

    Implementet ppt save to images functionality.

--- UnifiedOffice.PowerPoint.Tests/PresentationTest.cs
+++ UnifiedOffice.PowerPoint.Tests/PresentationTest.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 
@@ -10,5 +11,73 @@ namespace UnifiedOffice.PowerPoint.Tests
     public class PresentationTest
     {
         Application pptApp = null;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            this.pptApp = new Application();
+
+            string projectDirectory = this.GetProjectDirectory();
+            DirectoryInfo projectDirectoryInfo = new DirectoryInfo(projectDirectory + @"\misc\images");
+
+            foreach (FileInfo file in projectDirectoryInfo.GetFiles())
+            {
+                file.Delete();
+            }
+
+            foreach (DirectoryInfo dir in projectDirectoryInfo.GetDirectories())
+            {
+                dir.Delete(true);
+            }
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            this.pptApp.Quit();
+
+            this.pptApp = null;
+        }
+
+        [TestMethod]
+        public void Test_SaveToImages()
+        {
+            // Prepare
+
+            // Act
+            string projectDirectory = this.GetProjectDirectory();
+            Presentation openedPPT = this.pptApp.OpenPresentation(projectDirectory + @"\misc\1.pptx", true, false, false);
+
+            openedPPT.SaveToImages(projectDirectory + @"\misc\images\");
+
+            var files = Directory.GetFiles(projectDirectory + @"\misc\images\");
+
+            // Assert
+            Assert.AreEqual(1, files.Count());
+        }
+
+        [TestMethod]
+        public void Test_SaveToImages2()
+        {
+            // Prepare
+
+            // Act
+            string projectDirectory = this.GetProjectDirectory();
+            Presentation openedPPT = this.pptApp.OpenPresentation(projectDirectory + @"\misc\2.ppt", true, false, false);
+
+            openedPPT.SaveToImages(projectDirectory + @"\misc\images\");
+
+            var files = Directory.GetFiles(projectDirectory + @"\misc\images\");
+
+            // Assert
+            Assert.AreEqual(37, files.Count());
+        }
+
+        private string GetProjectDirectory()
+        {
+            string projectDirectory = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName;
+
+            return projectDirectory;
+        }
     }
 }
--- UnifiedOffice.PowerPoint/Application.cs
+++ UnifiedOffice.PowerPoint/Application.cs
@@ -13,10 +13,32 @@ namespace UnifiedOffice.PowerPoint
     public class Application
     {
         private InteropPowerPoint.Application app = null;
+        private IList<Presentation> presentations = new List<Presentation>();
 
         public Application()
         {
             this.app = new InteropPowerPoint.Application();
+        }
+
+        public Presentation OpenPresentation(string fileName, bool isReadOnly = true, bool isConfirmConvention = false, bool isVisible = true)
+        {
+            var isReadOnlyMsi = Microsoft.Office.Core.MsoTriState.msoTrue;
+            if (!isReadOnly)
+            {
+                isReadOnlyMsi = Microsoft.Office.Core.MsoTriState.msoFalse;
+            }
+
+            var isVisibleMsi = Microsoft.Office.Core.MsoTriState.msoTrue;
+            if (!isVisible)
+            {
+                isVisibleMsi = Microsoft.Office.Core.MsoTriState.msoFalse;
+            }
+
+            InteropPowerPoint.Presentation openedInteropPresentation = this.app.Presentations.Open(fileName, isReadOnlyMsi, Microsoft.Office.Core.MsoTriState.msoFalse, isVisibleMsi);
+
+            Presentation openedPresentation = this.GetUOPresentation(openedInteropPresentation);
+
+            return openedPresentation;
         }
 
         public void Quit()
@@ -28,6 +50,27 @@ namespace UnifiedOffice.PowerPoint
                 Marshal.ReleaseComObject(this.app);
                 this.app = null;
             }
+        }
+
+        private Presentation GetUOPresentation(InteropPowerPoint.Presentation interopPresentation)
+        {
+            Presentation foundPresentation = null;
+            foreach (var p in this.presentations)
+            {
+                if (p.InteropEquals(interopPresentation))
+                {
+                    foundPresentation = p;
+                    break;
+                }
+            }
+
+            if (foundPresentation == null)
+            {
+                foundPresentation = new Presentation(interopPresentation);
+                this.presentations.Add(foundPresentation);
+            }
+
+            return foundPresentation;
         }
     }
 }
